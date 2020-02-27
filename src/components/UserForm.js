b8100912e4c9b52103402bcb0646f53174c777c0
Josiah Roa <josiahroa18@gmail.com>
2020-02-27 14:31:33 -0700

    Added display users after submit functionality

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,53 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 
-function UserForm({ touched, errors }){
+function UserForm({ touched, errors, status }){
+    const [users, setUsers ] = useState([]);
+    useEffect(() => {
+        status && setUsers(users => [...users, status]);
+    }, [status]);
     return(
-        <div className='form-container'>
-            <h1>User Sign Up</h1>
-            <Form>
-                <label htmlFor='name'>Name</label>
-                <Field 
-                    id='name'
-                    type='text'
-                    name='name'
-                    placeholder='name'
-                />
-                {touched.name && errors.name && (
-                    <p className='error'>{errors.name}</p>
-                )}
-                <label htmlFor='Email'>Email</label>
-                <Field 
-                    id='email'
-                    type='email'
-                    name='email'
-                    placeholder='email'
-                />
-                {touched.email && errors.email && (
-                    <p className='error'>{errors.email}</p>
-                )}
-                <label htmlFor='Password'>Password</label>
-                <Field 
-                    id='password'
-                    type='password'
-                    name='password'
-                    placeholder='password'
-                />
-                {touched.password && errors.password && (
-                    <p className='error'>{errors.password}</p>
-                )}
-                <div className='tos-container'>
+        <div className='page'>
+            <div className='form-container'>
+                <h1>User Sign Up</h1>
+                <Form>
+                    <label htmlFor='name'>Name</label>
                     <Field 
-                        id='tos'
-                        type='checkbox'
-                        name='tos'
+                        id='name'
+                        type='text'
+                        name='name'
+                        placeholder='name'
                     />
-                    <p>Read terms of service</p>
-                </div>
-                <button type='submit'>Submit</button>
-            </Form>
+                    {touched.name && errors.name && (
+                        <p className='error'>{errors.name}</p>
+                    )}
+                    <label htmlFor='Email'>Email</label>
+                    <Field 
+                        id='email'
+                        type='email'
+                        name='email'
+                        placeholder='email'
+                    />
+                    {touched.email && errors.email && (
+                        <p className='error'>{errors.email}</p>
+                    )}
+                    <label htmlFor='Password'>Password</label>
+                    <Field 
+                        id='password'
+                        type='password'
+                        name='password'
+                        placeholder='password'
+                    />
+                    {touched.password && errors.password && (
+                        <p className='error'>{errors.password}</p>
+                    )}
+                    <div className='tos-container'>
+                        <Field 
+                            id='tos'
+                            type='checkbox'
+                            name='tos'
+                        />
+                        <p>Read terms of service</p>
+                    </div>
+                    <button type='submit'>Submit</button>
+                </Form>
+            </div>
+            <div className='current-users'>
+                {users.map(user => {
+                    return <div key={user.id}>
+                        <h3>{user.name}</h3>
+                        <p>{user.email}</p>
+                    </div>
+                })}
+            </div>
         </div>
     )
 }
@@ -59,7 +73,7 @@ const FormikUserForm = withFormik({
             name: name || '',
             email: email || '',
             password: password || '',
-            tos: tos || false
+            tos: tos || false,
         }
     },
     validationSchema: Yup.object().shape({
@@ -67,11 +81,12 @@ const FormikUserForm = withFormik({
         email: Yup.string().required('Email required'),
         password: Yup.string().required('Password required')
     }),
-    handleSubmit(values, { resetForm }){
+    handleSubmit(values, { resetForm, setStatus }){
         console.log('submitting', values);
         axios.post('https://reqres.in/api/users', values)
         .then(res => {
             console.log('Success:', res.data);
+            setStatus(res.data);
             resetForm();
         })
     }
